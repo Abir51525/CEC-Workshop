87c97f77c5f618d068c4efb43da239ff2e44be67
shahriar <shahriar.abir@northouth.edu>
2019-02-14 20:51:46 +0600

    initial commit

---
+++ src/Practice.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Practice {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int A, B, X;
+
+        A = input.nextInt();
+        B = input.nextInt();
+
+        X = A + B;
+
+        System.out.println(X);
+        input.close();
+    }
+}
